43e7cae9a8e770988074baeb971aa9006f4960d6
German Solano <kontrolg1239@gmail.com>
2020-08-28 17:52:10 -0400

    Add CharactersContext

---
+++ src/contexts/CharactersContext.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useState, useEffect, memo } from "react";
+
+const defaultState = {
+  characterNameQuery: "",
+  setcharacterNameQuery: () => {},
+  results: [],
+  setResults: () => {},
+  favoriteCharactersIds: []
+};
+
+const CharactersContext = createContext(defaultState);
+
+const CharactersProvider = ({ children }) => {
+  const [characterNameQuery, setCharacterNameQuery] = useState(
+    defaultState.characterNameQuery
+  );
+  useEffect(() => console.log(characterNameQuery), [characterNameQuery])
+  const [results, setResults] = useState(defaultState.results);
+
+  const contextValue = {
+    characterNameQuery,
+    setCharacterNameQuery,
+    results,
+    setResults
+  };
+
+  return (
+    <CharactersContext.Provider value={contextValue}>
+      {children}
+    </CharactersContext.Provider>
+  );
+};
+
+export default CharactersContext;
+
+const memoizedProvider = memo(CharactersProvider);
+
+export { memoizedProvider as CharactersProvider };
