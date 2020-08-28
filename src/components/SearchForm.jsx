427f78eafc31d601f752be84650ac57cb1f149b7
German Solano <kontrolg1239@gmail.com>
2020-08-28 18:20:51 -0400

    Add SearchForm character query logic

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useContext, useState, useCallback } from "react";
 import styled from "styled-components";
-import Icon from "./Icon";
 import Button from "./Button";
+import CharactersContext from "../contexts/CharactersContext";
 
 const StyledInput = styled.input`
   border: none;
@@ -9,8 +9,26 @@ const StyledInput = styled.input`
 `;
 
 const SearchForm = () => {
+  const { characterNameQuery, setCharacterNameQuery } = useContext(
+    CharactersContext
+  );
+  const [inputValue, setInputValue] = useState(characterNameQuery);
+
+  const changeInputValue = ({ target }) => {
+    const inputValue = target.value;
+    setInputValue(inputValue);
+  };
+
+  const changeCharacterNameQuery = useCallback(
+    (event) => {
+      event.preventDefault();
+      setCharacterNameQuery(inputValue);
+    },
+    [inputValue]
+  );
+
   return (
-    <form className="top-bar__search" action="#">
+    <form className="top-bar__search" onSubmit={changeCharacterNameQuery}>
       <Button
         className="top-bar__search__button"
         icon={{ name: "search", size: "1.25rem" }}
@@ -19,6 +37,8 @@ const SearchForm = () => {
         type="search"
         className="top-bar__search__input"
         name="search-superhero"
+        value={inputValue}
+        onChange={changeInputValue}
         id="search-superhero"
         placeholder="Buscar"
         title="Buscar personaje por su nombre"
