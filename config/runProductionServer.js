f698f786f53ed83244311d1cf51e5205a23e3077
German Solano <kontrolg1239@gmail.com>
2020-08-30 12:56:28 -0400

    Remove script running Webpack Interface

--- config/runProductionServer.js
+++ config/runProductionServer.js
@@ -1,8 +1,27 @@
-const app = require("./productionServerApp");
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.production");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-webpack(webpackConfig).run(() => {
-  const port = process.env.PORT || 7777;
-  app.listen(port, () => console.log(`App listening at port ${port}`));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/*", (request, response) => {
+  const filePath = getFilePath(request.url);
+  console.log(request.url, path.join(__dirname, "../public", filePath));
+  return response.sendFile(path.join(__dirname, "../public", filePath));
 });
+
+const getFilePath = (requestUrl) => {
+  const { dir, ext, base } = path.parse(requestUrl);
+  if (!ext) {
+    return "index.html";
+  }
+  return getFilePathFromRoot(dir, base);
+};
+
+const getFilePathFromRoot = (dir, base) => {
+  const pathSegments = dir.split("/").filter(Boolean).slice(1);
+  return path.join(...pathSegments, base);
+};
+
+const port = process.env.PORT || 7777;
+app.listen(port, () => console.log(`App listening at port ${port}`));
