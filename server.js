1db8a4abed20fbaba8ede1c92ef4e984ea7a03a1
German Solano <kontrolg1239@gmail.com>
2020-08-30 10:29:34 -0400

    Add getClientEnviromentVariales

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const path = require("path");
+const getClientEnviromentVariables = require("./webpack/getClientEnviromentVariables");
 const app = express();
+
+console.log(getClientEnviromentVariables());
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/*", (request, response) => {
-  response.sendFile(path.join(__dirname, "public", "index.html"));
+  const filePath = getFilePath(request.url);
+  return response.sendFile(path.join(__dirname, "public", filePath));
 });
 
 const port = process.env.PORT || 7777;
 
 app.listen(port, () => console.log(`App listening at port ${port}`));
+
+function getFilePath(requestUrl) {
+  const { dir, ext, base } = path.parse(requestUrl);
+  if (!ext) {
+    return "index.html";
+  }
+  return getFilePathFromRoot(dir, base);
+}
+
+function getFilePathFromRoot(dir, base) {
+  const pathSegments = dir.split("/").filter(Boolean).slice(1);
+  return path.join(...pathSegments, base);
+}
