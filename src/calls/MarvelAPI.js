5ac99dcbbdd8f79282b2d44cceac2aff7742d519
German Solano <kontrolg1239@gmail.com>
2020-08-30 14:22:20 -0400

    Modify MarvelAPI calls

--- src/calls/MarvelAPI.js
+++ src/calls/MarvelAPI.js
@@ -1,44 +1,36 @@
 import Axios from "axios";
 import md5 from "md5";
 
-const privateKey = process.env.REACT_APP_MARVEL_API_PRIVATE_KEY;
-const publicKey = process.env.REACT_APP_MARVEL_API_PUBLIC_KEY;
+const baseEndpoint = "https://gateway.marvel.com/v1/public";
+const charactersEndpoint = `${baseEndpoint}/characters`;
+
+const requestToCharactersEndpoint = (params = {}) => {
+  const authenticationParams = getAuthenticationParameters();
+
+  return Axios.get(charactersEndpoint, {
+    params: {
+      ...authenticationParams,
+      ...params
+    }
+  });
+};
 
 const getAuthenticationParameters = () => {
+  const privateKey = process.env.REACT_APP_MARVEL_API_PRIVATE_KEY;
+  const publicKey = process.env.REACT_APP_MARVEL_API_PUBLIC_KEY;
   const timestamp = Date.now();
   const hash = md5(timestamp + privateKey + publicKey);
   return { ts: timestamp, apikey: publicKey, hash };
 };
 
-const baseEndpoint = "https://gateway.marvel.com/v1/public";
-const charactersEndpoint = `${baseEndpoint}/characters`;
-
-const getCharacters = () => {
-  const authenticationParams = getAuthenticationParameters();
-
-  return Axios.get(charactersEndpoint, {
-    params: authenticationParams
-  });
-};
-
-const getCharacterByName = (name) => {
-  const authenticationParams = getAuthenticationParameters();
-
-  return Axios.get(charactersEndpoint, {
-    params: {
-      name,
-      ...authenticationParams
-    }
-  });
-};
+const getCharacters = () => requestToCharactersEndpoint();
+const getCharacterByName = (name) => requestToCharactersEndpoint({ name });
 
 const getCharacterById = (characterId) => {
   const authenticationParams = getAuthenticationParameters();
 
   return Axios.get(`${charactersEndpoint}/${characterId}`, {
-    params: {
-      ...authenticationParams
-    }
+    params: authenticationParams
   });
 };
 
