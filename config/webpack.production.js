9941086b68aea05c8d1bf1e402f7c26618a913d8
German Solano <kontrolg1239@gmail.com>
2020-08-30 13:08:06 -0400

    Add source map to staging configuration

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -2,8 +2,11 @@ const path = require("path");
 const commonConfig = require("./webpack.common");
 const { merge } = require("webpack-merge");
 
+const { NODE_ENV } = process.env;
+
 module.exports = merge(commonConfig, {
   mode: "production",
+  devtool: NODE_ENV === "staging" ? "source-map" : false,
   entry: {
     polyfill: "@babel/polyfill",
     app: "./src/index.js"
