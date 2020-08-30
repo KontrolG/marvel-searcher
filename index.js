dad48335359ddcef9b4f87439ae31f9555c6a1c9
German Solano <kontrolg1239@gmail.com>
2020-08-30 17:37:29 -0400

    Rename root index to differenciate from src/index

--- index.js
+++
@@ -1,7 +0,0 @@
-const NODE_ENV = process.env.NODE_ENV || "development";
-
-if (NODE_ENV === "development") {
-  require("./config/runDevelopmentServer");
-} else {
-  require("./config/runProductionServer");
-}
